968239cb8111a7d0797ae34a6b71593170c367c2
Yogiraj Mahale <yogirajmahale566@gmail.com>
2025-04-30 14:11:04 +0530

    final - commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://yogiraj-ticket.onrender.com/api";
 
 const api = axios.create({
   baseURL: API_URL,
